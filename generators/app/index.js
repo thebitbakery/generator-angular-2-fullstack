ad25a470fdd97d81ed597300fbc85ceb65526adb
Wung <wung@bitbakery.io>
2016-04-28 17:26:07 +0530

    add in the prompt questions

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,21 +14,47 @@ module.exports = yeoman.Base.extend({
     ));
 
     var prompts = [{
+      type: 'list',
+      name: 'markup',
+      message: 'What would you like to write markup with?',
+      choices: ['HTML', 'Jade'],
+      filter: val => val.toLowerCase()
+    },{
+      type: 'list',
+      name: 'stylesheet',
+      default: 1,
+      message: 'What would you like to write stylesheets with?',
+      choices: ['CSS', 'Sass', 'Stylus', 'Less'],
+      filter: val => val.toLowerCase()
+    }, {
+      type: 'list',
+      name: 'router',
+      default: 1,
+      message: 'What Angular router would you like to use?',
+      choices: ['ngRoute', 'uiRouter'],
+      filter: val => val.toLowerCase()
+    }, {
       type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+      name: 'bootstrap',
+      message: 'Would you like to include Bootstrap?'
+    }, {
+      type: 'confirm',
+      name: 'uibootstrap',
+      message: 'Would you like to include UI Bootstrap?',
+      when: answers => answers.bootstrap
+    }
+    ];
 
     this.prompt(prompts, function (props) {
       this.props = props;
       // To access props later use this.props.someAnswer;
-
       done();
     }.bind(this));
   },
 
   writing: function () {
+    // console.log('Answer has: ');
+    // console.log(this.props);
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
